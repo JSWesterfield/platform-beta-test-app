6ecb59dc46d1406e90a3357f382b01bfd3d3c900
Jake Westerfield <Jacob_Westerfield@discovery.com>
2022-01-20 18:13:47 -0500

    refactors code to handle import statments from require statements. As well as installed mongoose, dotenv, http, and express

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+import express from 'express'
 import http from 'http';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import * as log from 'https://deno.land/std@0.122.0/log/mod.ts';
 
+import toDo from './models/todo';
+
+require('dotenv').config();
+
+const app = express();
+
+console.log(toDo);
 
 const hostname = '127.0.0.1';
 const port = 3500;
